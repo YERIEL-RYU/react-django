a80e52e24fc599f7381fff1c81a2a006288639a2
YERIEL RYU <66396615+YERIEL-RYU@users.noreply.github.com>
2021-03-17 15:38:41 +0900

    backend blog migration

--- blogBackend/blog/models.py
+++ blogBackend/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    body = models.TextField()
+    important = models.BooleanField()
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    writer = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.title
--- blogBackend/blog/views.py
+++ blogBackend/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins, generics
+from rest_framework.response import Response
+from .models import Post
 
 # Create your views here.
+
+
+class PostViewSet(mixins.UpdateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin):
+    queryset = Post.objects.all()
