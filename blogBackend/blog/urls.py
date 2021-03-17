ba8df1f5f6c9d552b24a843b72f63cc17f57f017
YERIEL RYU <66396615+YERIEL-RYU@users.noreply.github.com>
2021-03-17 15:57:24 +0900

    [back] DRF mixin view 적용

--- blogBackend/blog/urls.py
+++ blogBackend/blog/urls.py
@@ -1,7 +1,13 @@
 # urls.py
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
-urlpatterns = [
+app_name = 'post'
 
+post_router = DefaultRouter()
+post_router.register(r'', views.PostViewSet)
+
+urlpatterns = [
+    path('', include(post_router.urls))
 ]
--- blogBackend/blog/views.py
+++ blogBackend/blog/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from rest_framework import viewsets, mixins, generics
 from rest_framework.response import Response
 from .models import Post
-
+from .serializers import PostSerializer
 # Create your views here.
 
 
-class PostViewSet(mixins.UpdateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin):
+class PostViewSet(mixins.UpdateModelMixin,
+                  mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.CreateModelMixin,
+                  viewsets.GenericViewSet):
     queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = None
